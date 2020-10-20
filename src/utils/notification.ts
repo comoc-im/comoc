a1ff700fe7f201522fe7950b1d765513464e774d
naeemo <370552604@qq.com>
2020-10-20 22:14:58 +0800

    feat base notification
    Signed-off-by: naeemo <370552604@qq.com>

---
+++ src/utils/notification.ts
@@ -0,0 +1,28 @@
+import {error} from "@/logger";
+
+export function notice (msg: string) {
+
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+        error("This browser does not support desktop notification");
+    }
+
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+        // If it's okay let's create a notification
+        new Notification(msg);
+    }
+
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function (permission) {
+            // If the user accepts, let's create a notification
+            if (permission === "granted") {
+                new Notification("Hi there!");
+            }
+        });
+    }
+
+    // At last, if the user has denied notifications, and you
+    // want to be respectful there is no need to bother them any more.
+}

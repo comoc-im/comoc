99c31a9a665211e4ce70fb23da0801a0ae21bae8
Naeemo <naeemo@qq.com>
2021-11-28 09:51:51 +0800

    config default beacon server url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const dbVersion = 6
 export const dbName = 'comoc'
-export const signalerServerWebSocketUrl = 'wss://api.comoc.chummy.fun'
+export const signalerServerWebSocketUrl = 'wss://beacon.comoc.chummy.fun'
 export const dataChannelLabel = 'Comoc IM'

a529c6866ad27613d625f4295d9492457a6f1134
Naeemo <naeemo@qq.com>
2022-01-09 15:37:11 +0800

    config add electron dev preview

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        minWidth: 800,
+        height: 600,
+        minHeight: 600,
+    })
+
+    win.loadURL('http://localhost:3000')
+}
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})

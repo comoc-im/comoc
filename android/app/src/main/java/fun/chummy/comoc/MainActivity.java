8948bc51a574caee024c8ae2779d13c8c1f8509a
Naeemo <naeemo@qq.com>
2022-04-14 21:48:30 +0800

    config add iOS/Android setup, using capacitorjs

---
+++ android/app/src/main/java/fun/chummy/comoc/MainActivity.java
@@ -0,0 +1,5 @@
+package fun.chummy.comoc;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+    appId: 'fun.chummy.comoc',
+    appName: 'comoc',
+    webDir: 'dist',
+    bundledWebRuntime: false,
+}
+
+export default config
